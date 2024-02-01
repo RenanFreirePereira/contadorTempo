db56b99d0a0542d33d87e97c26d3af81a6de7b5e
RenanFreirePereira <60330694+RenanFreirePereira@users.noreply.github.com>
2024-02-01 02:15:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const countdownElement = document.getElementById('contadorAAAA');
+
+    function updateCountdown() {
+        const currentDate = new Date();
+        const targetDate = new Date('February 5, 2024 07:10:00');
+        const timeDifference = targetDate - currentDate;
+        if (timeDifference > 0) {
+            let dias = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            let hora = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutos = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            let segundos = Math.floor((timeDifference % (1000 * 60)) / 1000);
+            if(hora < 10){
+                hora = "0" + hora
+            }
+            if(minutos < 10){
+                minutos = "0" + minutos
+            }
+            if(dias < 10){
+                dias = "0" + dias
+            }
+            if(segundos < 10){
+                segundos = "0" + segundos; 
+            }
+            countdownElement.innerHTML = `${dias} <sub>Dias</sub> : ${hora} : ${minutos} : ${segundos}`;
+        } else {
+            countdownElement.innerHTML = 'ACABOOOOOOOU! TEM AULA!';
+        }
+    }
+
+    setInterval(updateCountdown, 1000);
+
+    updateCountdown();
+});
